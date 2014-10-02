0a01db244c90f0346ebe46ba4631f80a722c4981
Dangeresque <seanmaclane@seans-air.biola.edu>
2014-10-01 18:57:15 -0700

    [add] gaussJordan

---
+++ gaussJordan.m
@@ -0,0 +1,21 @@
+function x = gaussJordan(A, b)
+
+[rA cA] = size(A);
+[rb cb] = size(b);
+
+M = [A b];
+
+for j = 1:cA
+    M(j, :) = M(j, :) / M(j, j);
+    
+    for i = 1:rA
+        if i == j
+            continue
+        end
+        M(i,:) =  M(i,:) - ((M(i,j) / M(j,j)) * M(j,:));
+        
+    end
+end
+
+x = M(:, cA+cb);
+end
