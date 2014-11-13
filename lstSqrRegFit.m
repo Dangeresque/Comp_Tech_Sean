34b7ac244b9fb8e9b61142d4a0617b42b2dab52c
Dangeresque <seanmaclane@seans-air.biola.edu>
2014-11-12 21:43:08 -0800

    [fix] lstSrwFit

--- lstSqrRegFit.m
+++ lstSqrRegFit.m
@@ -1,4 +1,5 @@
-function y = lstSqrRegFit(x, y)
+function yfit = lstSqrRegFit(x, y)
+%Returns an anonymous function of the line fit.
 
 Sx = sum(x); Sy = sum(y);
 Sxy = sum(x.*y); Sxx = sum(x.^2);
@@ -7,6 +8,7 @@ n = size(x, 2);
 m = (n*Sxy - Sx*Sy)/(n*Sxx - Sx.^2)
 b = (Sxx*Sy - Sxy*Sx)/(n*Sxx - Sx.^2)
 
-y = @(x) m*x + b;
+yfit = @(x) m*x + b;
 
+OverallError = sum((y - (m*x + b)).^2)
 end
