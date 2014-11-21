82d680a2610eb26eb70e0eec705558cd2cdd186d
Dangeresque <seanmaclane@seans-air.biola.edu>
2014-11-20 17:43:30 -0800

    [cleanup] LinReg

--- LinReg.m
+++ LinReg.m
@@ -4,8 +4,8 @@ Sx = sum(x); Sy = sum(y);
 Sxy = sum(x.*y); Sxx = sum(x.^2);
 n = size(x, 2);
 
-m = (n*Sxy - Sx*Sy)/(n*Sxx - Sx.^2)
-b = (Sxx*Sy - Sxy*Sx)/(n*Sxx - Sx.^2)
+m = (n*Sxy - Sx*Sy)/(n*Sxx - Sx.^2);
+b = (Sxx*Sy - Sxy*Sx)/(n*Sxx - Sx.^2);
 
 a = [m b];
 
